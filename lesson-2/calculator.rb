46c1e2d5d7a8e5d5cad4b39af980ff396a8c445f
gitvar <benvanderm@gmail.com>
2016-02-24 21:28:52 +0200

    First commit

---
+++ lesson-2/calculator.rb
@@ -0,0 +1,37 @@
+ # calculator.rb
+
+ # Build a command line calculator program that does the following:
+ #
+ #     asks for two numbers
+ #     asks for the type of operation to perform: add, subtract, multiply or divide
+ #     displays the result
+ #
+ # Use the Kernel.gets() method to retrieve user input, and use Kernel.puts() method to display output. Remember that Kernel.gets() includes the newline, so you have to call chomp() to remove it: Kernel.gets().chomp().
+
+ Kernel.puts("Hi, I am a calculator program! Please give me two numbers...")
+
+ Kernel.puts("What is the first number?")
+ number1 = Kernel.gets().chomp()
+
+ Kernel.puts("What is the 2nd number?")
+ number2 = Kernel.gets().chomp()
+
+ Kernel.puts()
+ Kernel.puts("What operation is required? : 1) +  2) -  3) *  4) / ")
+ operator = Kernel.gets().chomp()
+
+ case
+ when operator == '1'
+   answer = number1.to_i() + number2.to_i()
+ when operator == '2'
+   answer = number1.to_i() - number2.to_i()
+ when operator == '3'
+   answer = number1.to_i() * number2.to_i()
+ when operator == '4'
+   answer = number1.to_f() / number2.to_f()
+ else
+   Kernel.puts("I cannot perform that operation, sorry!")
+   exit
+ end
+
+Kernel.puts("The answer is #{answer}")

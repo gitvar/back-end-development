6504e191c153e4d0ec3a9d81351ddfdf6f1941b7
gitvar <benvanderm@gmail.com>
2016-02-27 21:43:55 +0200

    Updated as in Rubocop section in course.

--- lesson-2/calculator.rb
+++ lesson-2/calculator.rb
@@ -1,41 +1,102 @@
- # calculator.rb
+# calculator.rb
 
- # Build a command line calculator program that does the following:
- #
- #     asks for two numbers
- #     asks for the type of operation to perform: add, subtract, multiply or divide
- #     displays the result
- #
- # Use the Kernel.gets() method to retrieve user input, and use Kernel.puts() method to display output. Remember that Kernel.gets() includes the newline, so you have to call chomp() to remove it: Kernel.gets().chomp().
+# Build a command line calculator program that does the following:
+#
+#     asks for two numbers
+#     asks for the type of operation to perform: add, subtract, multiply or divide
+#     displays the result
+#
+# Use the Kernel.gets() method to retrieve user input, and use Kernel.puts() method to display
+# output. Remember that Kernel.gets() includes the newline, so you have to call chomp() to remove
+# it: Kernel.gets().chomp().
 
 require 'pry'
 
- Kernel.puts("Hi, I am a calculator program! Please give me two numbers...")
+def prompt(message = " ")
+  Kernel.puts("=> #{message}")
+end
 
- Kernel.puts("What is the first number?")
- number1 = Kernel.gets().chomp()
+def valid?(num)
+  num.to_i() != 0
+end
 
- Kernel.puts("What is the 2nd number?")
- number2 = Kernel.gets().chomp()
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
 
- Kernel.puts()
- Kernel.puts("What operation is required? : 1) +  2) -  3) *  4) / ")
- operator = Kernel.gets().chomp()
+name = ''
+number1 = ''
+number2 = ''
+operator = ''
 
- case
- when operator == '1'
-   answer = number1.to_i() + number2.to_i()
- when operator == '2'
-   answer = number1.to_i() - number2.to_i()
- when operator == '3'
-   answer = number1.to_i() * number2.to_i()
- when operator == '4'
-   answer = number1.to_f() / number2.to_f()
- else
-   Kernel.puts("I cannot perform that operation, sorry!")
-   exit
- end
+prompt("Hi, I am a calculator program! Please enter your name.")
+loop do
+  name = Kernel.gets().chomp()
+  break unless name.empty?
+  prompt("Hmmm... Please anter a valid name.")
+end
 
-# binding.pry
+prompt("Hello #{name}, please give me two numbers to work with:")
 
-Kernel.puts("The answer is #{answer}")
+loop do
+  loop do
+    prompt("What is the first number?")
+    number1 = Kernel.gets().chomp()
+    break if valid?(number1)
+    prompt("Hmmm... That doesn't look right. Try again ...")
+  end
+
+  loop do
+    prompt("What is the second number?")
+    number2 = Kernel.gets().chomp()
+    break if valid?(number2)
+    prompt("Hmmm... That doesn't look right. Try again ...")
+  end
+
+  prompt("What operation is required? : 1) +  2) -  3) *  4) / ")
+  operator_prompt = <<~MSG
+  What operation is required?
+    1) Addition
+    2) Subtraction
+    3) Multiplication
+    4) Division
+   MSG
+
+  prompt(operator_prompt)
+  loop do
+    operator = Kernel.gets().chomp()
+    break if %w(1 2 3 4).include?(operator)
+    prompt("Choose only 1, 2, 3 or 4 please #{name}.")
+  end
+
+  prompt("#{operation_to_message(operator)} the numbers #{number1} and #{number2}")
+
+  answer =  case operator
+            when '1'
+              number1.to_i() + number2.to_i()
+            when '2'
+              number1.to_i() - number2.to_i()
+            when '3'
+              number1.to_i() * number2.to_i()
+            when '4'
+              number1.to_f() / number2.to_f()
+            end
+
+  # binding.pry
+
+  prompt("The answer is #{answer}")
+  prompt("Again? (Y) to continue")
+  input = Kernel.gets().chomp()
+  break unless input.downcase().start_with?("y")
+end
+
+prompt("Thanks for using 'calulator.rb', #{name}. Goodbye!")

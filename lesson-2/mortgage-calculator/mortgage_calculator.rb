754b2c0b69b1c0d5293c7141e5569a42077d756a
gitvar <benvanderm@gmail.com>
2016-02-29 16:51:34 +0200

    First Commit

---
+++ lesson-2/mortgage-calculator/mortgage_calculator.rb
@@ -0,0 +1,22 @@
+Take everything you've learned so far and build a mortgage calculator (or car payment calculator -- it's the same thing).
+
+You'll need three pieces of information:
+
+    the loan amount
+    the Annual Percentage Rate (APR)
+    the loan duration
+
+From the above, you'll need to calculate the following two things:
+
+    monthly interest rate
+    loan duration in months
+
+You can then use the formula described here: http://www.mtgprofessor.com/formulas.htm
+
+Finally, don't forget to run your code through Rubocop.
+
+Hints:
+
+    Figure out what format your inputs need to be in. For example, should the interest rate be expressed as 5 or .05, if you mean 5% interest?
+    If you're working with Annual Percentage Rate (APR), you'll need to convert that to a monthly interest rate.
+    Be careful about the loan duration -- are you working with months or years? Choose variable names carefully to assist in remembering.

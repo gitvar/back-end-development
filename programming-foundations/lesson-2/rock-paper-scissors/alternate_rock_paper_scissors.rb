e0523b8e80b54060d091aff4c46caf5743469b06
gitvar <benvanderm@gmail.com>
2016-03-02 21:23:43 +0200

    rock_paper_sciccors: rubocop update and added win? method. alternate_rock_paper_scissors: implemented "calculate_result" method as well as "test_method". Added calls to test_method at different places.

---
+++ programming-foundations/lesson-2/rock-paper-scissors/alternate_rock_paper_scissors.rb
@@ -0,0 +1,47 @@
+# alternate_rock_paper_scissors.rb
+
+VALID_CHOICES = %w(rock paper scissors).freeze
+
+def test_method
+  prompt("test_method")
+end
+
+#test_method() # Causes error -> 'test_method': undefined method 'prompt'
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+# test_method()
+
+def calculate_result(player, computer)
+  if (player == "rock" && computer == "scissors") ||
+      (player == "scissors" && computer == "paper") ||
+      (player == "paper" && computer == "rock")
+    result = "You win!"
+  elsif player == computer
+    result = "It's a tie!"
+  else
+    result = "Computer wins!"
+  end
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt("Please choose one: #{VALID_CHOICES.join(", ")}")
+    choice = Kernel.gets().chomp().downcase()
+    break if VALID_CHOICES.include?(choice)
+    prompt("That's not a valid choice. Try again.")
+  end
+
+  computer_choice = VALID_CHOICES.sample
+  prompt("You chose: #{choice}; Computer chose: #{computer_choice}.")
+  puts calculate_result(choice, computer_choice)
+
+  prompt("Play again? (Y) to continue ...")
+  play_again = Kernel.gets().chomp().downcase()
+  break unless play_again.start_with?('y')
+end
+
+prompt("Thanks for playing. Goodbye!")
--- programming-foundations/lesson-2/rock-paper-scissors/rock_paper_scissors.rb
+++ programming-foundations/lesson-2/rock-paper-scissors/rock_paper_scissors.rb
@@ -1,40 +1,44 @@
 # rock_paper_scissors.rb
 
-VALID_CHOICES = ["rock", "paper", "scissors"].freeze
+VALID_CHOICES = %w(rock paper scissors).freeze
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def win?(player, computer)
+  (player == 'rock' && computer == 'scissors') ||
+    (player == 'scissors' && computer == 'paper') ||
+    (player == 'paper' && computer == 'rock')
+end
+
 def display_results(player, computer)
   prompt("You chose: #{player}; Computer chose: #{computer}.")
 
-  if (player == "rock" && computer == "scissors") ||
-      (player == "scissors" && computer == "paper") ||
-      (player == "paper" && computer == "rock")
-    prompt("You win!")
+  if win?(player, computer)
+    prompt('You win!')
   elsif player == computer
-    prompt("It's a tie!")
+    prompt('It\'s a tie!')
   else
-    prompt("Computer wins!")
+    prompt('Computer wins!')
   end
 end
 
 loop do
   choice = ''
   loop do
-    prompt("Please choose one: #{VALID_CHOICES.join(", ")}")
+    prompt("Please choose one: #{VALID_CHOICES.join(', ')}")
     choice = Kernel.gets().chomp().downcase()
     break if VALID_CHOICES.include?(choice)
-    prompt("That's not a valid choice. Try again.")
+    prompt('That\'s not a valid choice. Try again.')
   end
 
   computer_choice = VALID_CHOICES.sample
   display_results(choice, computer_choice)
 
-  prompt("Play again? (Y) to continue ...")
+  prompt('Play again? (Y) to continue ...')
   play_again = Kernel.gets().chomp().downcase()
   break unless play_again.start_with?('y')
 end
 
-prompt("Thanks for playing. Goodbye!")
+prompt('Thanks for playing. Goodbye!')

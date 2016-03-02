4f57d9c282ff1132e52c840e4a70e5919cf8ed70
gitvar <benvanderm@gmail.com>
2016-03-02 20:36:42 +0200

    First commit.

---
+++ programming-foundations/lesson-2/rock-paper-scissors/rock_paper_scissors.rb
@@ -0,0 +1,40 @@
+# rock_paper_scissors.rb
+
+VALID_CHOICES = ["rock", "paper", "scissors"].freeze
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def display_results(player, computer)
+  prompt("You chose: #{player}; Computer chose: #{computer}.")
+
+  if (player == "rock" && computer == "scissors") ||
+      (player == "scissors" && computer == "paper") ||
+      (player == "paper" && computer == "rock")
+    prompt("You win!")
+  elsif player == computer
+    prompt("It's a tie!")
+  else
+    prompt("Computer wins!")
+  end
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt("Please choose one: #{VALID_CHOICES.join(", ")}")
+    choice = Kernel.gets().chomp().downcase()
+    break if VALID_CHOICES.include?(choice)
+    prompt("That's not a valid choice. Try again.")
+  end
+
+  computer_choice = VALID_CHOICES.sample
+  display_results(choice, computer_choice)
+
+  prompt("Play again? (Y) to continue ...")
+  play_again = Kernel.gets().chomp().downcase()
+  break unless play_again.start_with?('y')
+end
+
+prompt("Thanks for playing. Goodbye!")

08747747c97939baaf7bda5ad673c14caea41426
gitvar <benvanderm@gmail.com>
2016-03-03 22:05:59 +0200

    lizard_spock.rb: 1. Added bonus features 1 and 2 so far.

--- programming-foundations/lesson-2/rock-paper-scissors/lizard_spock.rb
+++ programming-foundations/lesson-2/rock-paper-scissors/lizard_spock.rb
@@ -1,22 +1,32 @@
 # lizard_spock.rb
 
 VALID_CHOICES = %w(rock paper scissors lizard spock).freeze
+VALID_CHOICES_DISPLAY = %w((r)ock (p)aper s(c)issors (l)izard (s)pock).freeze
+VALID_ABBREVIATED_CHOICES = %w(r p c l s).freeze
+VALID_CHOICES_HASH = { 'r' => 'rock', 'p' => 'paper', 'c' => 'scissors',
+                       'l' => 'lizard', 's' => 'spock' }.freeze
+
+WINNING_COMBINATIONS = [%w(rock scissors),
+                        %w(scissors paper),
+                        %w(paper rock),
+                        %w(rock lizard),
+                        %w(lizard spock),
+                        %w(spock scissors),
+                        %w(scissors lizard),
+                        %w(lizard paper),
+                        %w(paper spock),
+                        %w(spock rock)].freeze
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
 def win?(player, computer)
-  (player == 'rock' && computer == 'scissors') ||
-    (player == 'scissors' && computer == 'paper') ||
-    (player == 'paper' && computer == 'rock') ||
-    (player == 'rock' && computer == 'lizard') ||
-    (player == 'lizard' && computer == 'spock') ||
-    (player == 'spock' && computer == 'scissors') ||
-    (player == 'scissorrs' && computer == 'lizard') ||
-    (player == 'lizard' && computer == 'paper') ||
-    (player == 'paper' && computer == 'spock') ||
-    (player == 'spock' && computer == 'rock') ||
+  WINNING_COMBINATIONS.each do |element_1, element_2|
+    return true if player == element_1 && computer == element_2
+  end
+
+  false
 end
 
 def display_results(player, computer)
@@ -34,14 +44,14 @@ end
 loop do
   choice = ''
   loop do
-    prompt("Please choose one: #{VALID_CHOICES.join(', ')}")
+    prompt("Please choose one: #{VALID_CHOICES_DISPLAY.join(', ')}")
     choice = Kernel.gets().chomp().downcase()
-    break if VALID_CHOICES.include?(choice)
+    break if VALID_ABBREVIATED_CHOICES.include?(choice)
     prompt('That\'s not a valid choice. Try again.')
   end
 
   computer_choice = VALID_CHOICES.sample
-  display_results(choice, computer_choice)
+  display_results(VALID_CHOICES_HASH[choice], computer_choice)
 
   prompt('Play again? (Y) to continue ...')
   play_again = Kernel.gets().chomp().downcase()

46fe375c15079e0ae13d79d78490c6f93d9a41c0
gitvar <benvanderm@gmail.com>
2016-03-04 12:31:03 +0200

    .rubocop.yml: enabled hash braces and single/double quote rules. lizard_spock: Took out redundant CHOICES constants, cleaned up decision code, changed how program ends (press 'Enter' to continue and 's' to stop).

--- programming-foundations/lesson-2/rock-paper-scissors/lizard_spock.rb
+++ programming-foundations/lesson-2/rock-paper-scissors/lizard_spock.rb
@@ -1,11 +1,9 @@
 # lizard_spock.rb
 
 VALID_CHOICES = %w(rock paper scissors lizard spock).freeze
-VALID_CHOICES_DISPLAY = %w((r)ock (p)aper s(c)issors (l)izard (s)pock).freeze
-VALID_ABBREVIATED_CHOICES = %w(r p c l s).freeze
+VALID_CHOICES_FOR_DISPLAY = %w((r)ock (p)aper s(c)issors (l)izard (s)pock).freeze
 VALID_CHOICES_HASH = { 'r' => 'rock', 'p' => 'paper', 'c' => 'scissors',
                        'l' => 'lizard', 's' => 'spock' }.freeze
-
 WINNING_COMBINATIONS = [%w(rock scissors),
                         %w(scissors paper),
                         %w(paper rock),
@@ -17,6 +15,14 @@ WINNING_COMBINATIONS = [%w(rock scissors),
                         %w(paper spock),
                         %w(spock rock)].freeze
 
+def display_valid_choices
+  prompt("Choose one by typing the letter in brackets: #{VALID_CHOICES_FOR_DISPLAY.join(', ')}:")
+end
+
+def valid?(choice)
+  VALID_CHOICES_HASH.key?(choice)
+end
+
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
@@ -25,38 +31,37 @@ def win?(player, computer)
   WINNING_COMBINATIONS.each do |element_1, element_2|
     return true if player == element_1 && computer == element_2
   end
-
   false
 end
 
 def display_results(player, computer)
-  prompt("You chose: #{player}; Computer chose: #{computer}.")
+  prompt("You chose: #{player.capitalize}; Computer chose: #{computer.capitalize}.")
 
   if win?(player, computer)
     prompt('You win!')
   elsif player == computer
-    prompt('It\'s a tie!')
+    prompt("It's a tie!")
   else
     prompt('Computer wins!')
   end
 end
 
+# Main Loop starts here:
 loop do
-  choice = ''
+  player_choice = ''
   loop do
-    prompt("Please choose one: #{VALID_CHOICES_DISPLAY.join(', ')}")
-    choice = Kernel.gets().chomp().downcase()
-    choice = choice[0]
-    break if VALID_ABBREVIATED_CHOICES.include?(choice)
-    prompt('That\'s not a valid choice. Try again.')
+    display_valid_choices()
+    player_choice = Kernel.gets().chomp().downcase().chr
+    break if valid?(player_choice)
+    prompt("That's not a valid choice. Try again.")
   end
 
   computer_choice = VALID_CHOICES.sample
-  display_results(VALID_CHOICES_HASH[choice], computer_choice)
+  display_results(VALID_CHOICES_HASH[player_choice], computer_choice)
 
-  prompt('Play again? (Y) to continue ...')
+  prompt("'Enter' to play again, 's' to stop.")
   play_again = Kernel.gets().chomp().downcase()
-  break unless play_again.start_with?('y')
+  break if play_again.start_with?('s')
 end
 
 prompt('Thanks for playing. Goodbye!')

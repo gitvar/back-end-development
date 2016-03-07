9b064c700a0bbc065a49a466d03a107ae4c2ecdf
gitvar <benvanderm@gmail.com>
2016-03-07 11:33:20 +0200

    Added 2nd option (same thing really!)

--- programming-foundations/lesson-3/easy-3/question_2.rb
+++ programming-foundations/lesson-3/easy-3/question_2.rb
@@ -2,8 +2,12 @@
 
 # How can we add the family pet "Dino" to our usual array:
 
+# 1:
 flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
-
 flintstones << 'Dino'
+p flintstones
 
+# 2:
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones.push('Dino')
 p flintstones

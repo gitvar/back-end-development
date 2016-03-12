67aee1807b3515a9de429584d0a0814accbd07da
gitvar <benvanderm@gmail.com>
2016-03-12 10:15:08 +0200

    Updated this method to contain all the examples of mutating the caller (or not) in the course so far. +, =, []=, <<

--- programming-foundations/lesson-3/medium-2/question_3_6.rb
+++ programming-foundations/lesson-3/medium-2/question_3_6.rb
@@ -1,26 +1,70 @@
 # question_3_6.rb
 
-# Relating to methods mutating the caller and not mutatingthe caller:
+def prefix(str)
+  "Mr " + str       # + does not mutate the caller.
+end
+def suffix(str)
+  str << "son"      # << mutates the caller.
+end
+name = "John"
+prefix(name)
+puts name
+suffix(name)
+puts name
+
+
+answer = 42
+def mess_with_it(some_number)
+  some_number += 8    # + and = do not mutate the caller.
+end
+new_answer = mess_with_it(answer)
+p answer - 10
+# Answer = 32.
 
 def tricky_method(a_string_param, an_array_param)
-  a_string_param += "rutabaga"
-  an_array_param += ["rutabaga"]
+  a_string_param += "rutabaga"      # = and + do not mutate the caller.
+  an_array_param += ["rutabaga"]    # = and + do not mutate the caller,
 end
-
 def my_tricky_method(a_string_param, an_array_param)
-  index = 3
-  a_string_param[index] += "*-*"
-  an_array_param[index] += "-*-"
+  index = 1
+  a_string_param[index] += "*-*"    # This is []= : Mutates the caller!
+  an_array_param[index] += "-*-"    # This is []= : Mutates the caller!
 end
-
 my_string = "apples"
-my_array = ["apples", "bananas", "oranges", "lemons"]
+my_array = ["apples", "bananas"]
 tricky_method(my_string, my_array)
-puts "My string looks like this now: #{my_string}"
-puts "My array looks like this now: #{my_array}"
-
+puts "My string looks like this now: #{my_string}"    # "apples"
+puts "My array looks like this now: #{my_array}"      # ["apples", "bananas"]
 my_string = "apples"
-my_array = ["apples", "bananas", "oranges", "lemons"]
+my_array = ["apples", "bananas"]
 my_tricky_method(my_string, my_array)
-puts "My string looks like this now: #{my_string}"
-puts "My array looks like this now: #{my_array}"
+puts "My string looks like this now: #{my_string}"    # "ap*-*ples"
+puts "My array looks like this now: #{my_array}"      # ["apples", "bananas-*-"]
+
+
+def tricky_method2(a_string_param, an_array_param)
+  a_string_param += "rutabaga"    # = and + do NOT mutate the caller.
+  an_array_param << "rutabaga"    # << mutates the caller.
+end
+puts my_string = "pumpkins"
+puts my_array = ["pumpkins"]
+tricky_method2(my_string, my_array)
+puts "My string looks like this now: #{my_string}"    # "pumpkins"
+puts "My array looks like this now: #{my_array}"      # ["pumkins", "rutabaga"]
+
+
+def tricky_method3(a_string_param, an_array_param)
+  a_string_param.gsub!('pumpkins', 'rutabaga')
+  an_array_param = ['pumpkins', 'rutabaga']
+end
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+tricky_method3(my_string, my_array)
+puts "My string looks like this now: #{my_string}"    # .gsub! mutates caller. "rutabaga"
+puts "My array looks like this now: #{my_array}"      # []= does NOT mutate. ["pumpkins"]
+
+# My Answer:
+# my_string = 'rutabaga' # .gsub!() mutates the caller. *Correct*
+# my_array = ['pumpkins', 'rutabaga'] # []= mutates the caller. *Wrong!*
+# above line is not []= with index but simple '=' which does NOT mutate the caller!
+# my_array = ["pumpkins"]

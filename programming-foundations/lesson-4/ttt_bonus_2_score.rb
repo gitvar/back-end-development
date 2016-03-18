24d5bd7daa6780df471e62bf87e7904e813102c9
gitvar <benvanderm@gmail.com>
2016-03-18 09:32:14 +0200

    ttt_bonus_1.rb : Added launch School solution. ttt_bonus_2_score.rb : First working attempt commit.

--- programming-foundations/lesson-4/ttt_bonus_2_score.rb
+++ programming-foundations/lesson-4/ttt_bonus_2_score.rb
@@ -1,6 +1,6 @@
-# ttt_bonus_1.rb
+# ttt_bonus_2_score.rb
 
-# Improved "join"
+# Keep score (no constants or globals). First one to reach 5, wins.
 
 require 'pry'
 
@@ -12,38 +12,56 @@ WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                 [[1, 5, 9], [3, 5, 7]].freeze
 SEPARATOR = ', '.freeze
 END_WORD = 'or'.freeze
+DISPLAY_OFFSET = '          '.freeze
+DISPLAY_UNDERLINE =        '-----------------'.freeze
+DISPLAY_DOUBLE_UNDERLINE = '================='.freeze
+PLAYER = 0.freeze
+COMPUTER = 1.freeze
+MAX_SCORE = 5
+
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+def display(display_string = '')
+  puts DISPLAY_OFFSET + display_string
+end
+
 # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
-def display_board(brd)
+def display_board(brd, scores, comment = '')
   system 'clear' # For Mac Terminal
-  puts
-  puts
-  puts 'Tic Tac Toe!'.center(27)
-  print '    '
-  17.times { print '=' }
-  puts
-  puts "       Player: #{PLAYER_MARKER}"
-  puts "       Computer: #{COMPUTER_MARKER}"
-  print '    '
-  17.times { print '-' }
-  puts '    '
-  puts '    '
-  puts '         |     |'
-  puts "      #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
-  puts '         |     |'
-  puts '    -----+-----+-----'
-  puts '         |     |'
-  puts "      #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
-  puts '         |     |'
-  puts '    -----+-----+-----'
-  puts '         |     |'
-  puts "      #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
-  puts '         |     |'
-  puts '    '
+  display
+  display
+  display 'Tic Tac Toe!'.center(18)
+  display
+  display(DISPLAY_DOUBLE_UNDERLINE)
+  display
+  display "Player: #{PLAYER_MARKER}"
+  display "Computer: #{COMPUTER_MARKER}"
+  display
+  display(DISPLAY_UNDERLINE)
+  display
+  display "Player Score: #{scores[PLAYER]}"
+  display "Computer Score: #{scores[COMPUTER]}"
+  display
+  display(DISPLAY_UNDERLINE)
+  display
+  display
+  display '     |     |'
+  display "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  display '     |     |'
+  display '-----+-----+-----'
+  display '     |     |'
+  display "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  display '     |     |'
+  display '-----+-----+-----'
+  display '     |     |'
+  display "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  display '     |     |'
+  display
+  display
+  display "#{comment}"
 end
 # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
@@ -97,20 +115,12 @@ def computer_places_piece!(brd)
   brd[square] = COMPUTER_MARKER
 end
 
-def board_full?(brd)
-  empty_squares(brd).empty? # same as empty_squares(brd) == []
-end
-
-def someone_won?(brd)
-  !!detect_winner(brd) # !! (bang bang) forces return value (string) to boolean,,,,,
-end
-
 def check_winner(brd, line, marker)
   # brd.values_at(line[0], line[1], line[2]).count(marker) == 3
   brd.values_at(*line).count(marker) == 3
 end
 
-def detect_winner(brd)
+def detect_winner(brd, scores)
   WINNING_LINES.each do |line|
     if check_winner(brd, line, PLAYER_MARKER)
       return "Player"
@@ -121,26 +131,52 @@ def detect_winner(brd)
   nil # return nil to force someone_won? to return false. "nil = false"
 end
 
+def board_full?(brd)
+  empty_squares(brd).empty? # same as empty_squares(brd) == []
+end
+
+def someone_won?(brd, scores)
+  !!detect_winner(brd, scores) # !! (bang bang) forces return value (string) to boolean ...
+end
+
+scores = [0, 0]
+comment = ''
 loop do
   board = initialize_board
   loop do
-    display_board(board)
+    display_board(board, scores)
     player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    break if someone_won?(board, scores) || board_full?(board)
     computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    break if someone_won?(board, scores) || board_full?(board)
   end
 
-  display_board(board)
-  if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
+  display_board(board, scores)
+  if someone_won?(board, scores)
+    if detect_winner(board, scores) == "Player"
+      scores[PLAYER] += 1
+      if scores[PLAYER] == MAX_SCORE
+        comment = "Player wins the Game!"
+      else
+        comment = "Player won the round!"
+      end
+    elsif detect_winner(board, scores) == "Computer"
+      scores[COMPUTER] += 1
+      if scores[COMPUTER] == MAX_SCORE
+        comment = "Computer wins the Game!"
+      else
+        comment = "Computer won the round!"
+      end
+    end
   else
-    prompt "It's a tie!"
+    comment = "It's a tie!"
   end
-  puts ""
+  display_board(board, scores, comment)
+  display
   prompt "Play again? (y or n)."
   continue = gets.chomp.downcase
   break if !continue.start_with?("y")
+  scores = [0, 0] if scores.include?(MAX_SCORE)
 end
 
 prompt "Goodbye, thanks for playing!"
